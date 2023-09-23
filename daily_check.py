a2ca578b1a6927d8a268ad5522b35d84714b00bb
Kyle-Home <kylewang2655@gmail.com>
2023-09-23 16:06:05 +0800

    update

--- daily_check.py
+++ daily_check.py
@@ -14,9 +14,9 @@ from oauth2client.service_account import ServiceAccountCredentials
 
 # Check the platform from the sys module
 if sys.platform.startswith('linux'):
-    cred_path = "/home/opc/.ssh/creds.json"
+    cred_path = "./creds.json"
 elif sys.platform.startswith('win'):
-    cred_path = "C:/Users/Kyle/.ssh/creds.json"
+    cred_path = "./creds.json"
 else:
     print("Unsupported platform. Exiting the script.")
     sys.exit()
