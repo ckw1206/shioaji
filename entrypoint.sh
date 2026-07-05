3dfbacb52707a7360dd57ed08274c7cdced29b3a
Kyle.W <kylewang2655@gmail.com>
2026-07-05 14:58:58 +0800

    feat: add Dockerfile and cron entrypoint for shioaji_sync.py

---
+++ entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+set -e
+
+: "${CRON_SCHEDULE:=0 9 * * 1-5}"
+
+echo "Installing cron schedule: ${CRON_SCHEDULE}"
+echo "${CRON_SCHEDULE} cd /app && /usr/local/bin/python /app/shioaji_sync.py >> /proc/1/fd/1 2>> /proc/1/fd/2" | crontab -
+
+exec cron -f
